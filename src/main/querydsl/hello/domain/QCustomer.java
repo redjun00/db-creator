bc972eecac346fbfb4af203421da17e96f9be90d
nrkim <spqjsWo>
2016-11-03 18:00:03 +0900

    혹시 몰라서 Q 파일 올림.

---
+++ src/main/querydsl/hello/domain/QCustomer.java
@@ -0,0 +1,41 @@
+package hello.domain;
+
+import static com.mysema.query.types.PathMetadataFactory.*;
+
+import com.mysema.query.types.path.*;
+
+import com.mysema.query.types.PathMetadata;
+import javax.annotation.Generated;
+import com.mysema.query.types.Path;
+
+
+/**
+ * QCustomer is a Querydsl query type for Customer
+ */
+@Generated("com.mysema.query.codegen.EntitySerializer")
+public class QCustomer extends EntityPathBase<Customer> {
+
+    private static final long serialVersionUID = 182726860L;
+
+    public static final QCustomer customer = new QCustomer("customer");
+
+    public final StringPath address = createString("address");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath name = createString("name");
+
+    public QCustomer(String variable) {
+        super(Customer.class, forVariable(variable));
+    }
+
+    public QCustomer(Path<? extends Customer> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QCustomer(PathMetadata<?> metadata) {
+        super(Customer.class, metadata);
+    }
+
+}
+
